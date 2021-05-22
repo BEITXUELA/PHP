1b2ec4d0b6699c2b36690e691371e0b7c9becad8
BEITXUELA <70911311+BEITXUELA@users.noreply.github.com>
2021-05-22 12:35:07 +0200

    Add files via upload

---
+++ 01_carga/index.php
@@ -0,0 +1,17 @@
+<?php 
+    echo "<h1>Bienvenid@s</h1>"
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BEA</title>
+</head>
+<body>
+    <h1>Ongi Etorri</h1>
+</body>
+</html>
